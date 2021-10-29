cc329a8f2fc3c8e7e811041680db77ad44ade2d2
Miguel Pazo <miguel.ps19@gmail.com>
2021-10-29 10:04:22 -0500

    fix filter auth

--- src/common/auth/authProvider.ts
+++ src/common/auth/authProvider.ts
@@ -21,12 +21,54 @@ export class AuthProvider implements interfaces.AuthProvider {
     @inject(TYPES.IJwtService)
     private readonly jwtService: IJwtService;
 
+    private readonly pathsJwt = ['/user'];
+    private readonly pathsToken = ['/upload'];
+
     public async getUser(req: e.Request, res: e.Response, next: e.NextFunction): Promise<interfaces.Principal> {
         try {
             // @ts-ignore
             const token = req.headers.authorization.split(this.authenticationSchemeName)[1];
+
+            if (this.pathsJwt.some(x => req.url.startsWith(x))) {
+                return await this.validateJwt(token);
+            }
+
+            if (this.pathsToken.some(x => req.url.startsWith(x))) {
+                return await this.validateToken(token);
+            }
+
+        } catch (err) {
+            this.logger.getLogger().error(err);
+        }
+
+        return new Principal(null);
+    }
+
+    public async validateJwt(token: string): Promise<interfaces.Principal> {
+        try {
             const user = await this.jwtService.verifyAuthToken(token);
-            return new Principal(user);
+
+            if (user) {
+                const isRevoked = await this.jwtService.getRevokedToken(token);
+
+                if (!isRevoked) {
+                    return new Principal(user);
+                }
+            }
+
+        } catch (err) {
+            this.logger.getLogger().error(err);
+        }
+
+        return new Principal(null);
+    }
+
+    public async validateToken(token: string): Promise<interfaces.Principal> {
+        try {
+            if (token === process.env.APP_ACCESS_TOKEN) {
+                return new Principal({app: true});
+            }
+
         } catch (err) {
             this.logger.getLogger().error(err);
         }
